6456534528eb46f2819e78afaf69e906db8f404e
Slawomir Was <was.slawek@gmail.com>
2024-02-19 11:59:41 +0100

    Added Database Tables and Migrations. End Implement Database Sections

---
+++ Management.Web/Data/BaseEntity.cs
@@ -0,0 +1,9 @@
+﻿namespace Management.Web.Data
+{
+	public abstract class BaseEntity
+	{
+		public int Id { get; set; }
+		public DateTime DateCreated { get; set; }
+		public DateTime DateModified { get; set; }
+	}
+}
