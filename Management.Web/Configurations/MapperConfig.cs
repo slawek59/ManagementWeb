5372f0e0c14cc5d764c97f1942aeb2005b33e93f
Slawomir Was <was.slawek@gmail.com>
2024-02-20 15:23:33 +0100

    Modified the crud screens and added datatables and swal.

---
+++ Management.Web/Configurations/MapperConfig.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Management.Web.Data;
+using Management.Web.Models;
+
+namespace Management.Web.Configurations
+{
+	public class MapperConfig : Profile
+	{
+		public MapperConfig()
+		{
+			CreateMap<LeaveType, LeaveTypeVM>().ReverseMap();
+		}
+	}
+}
